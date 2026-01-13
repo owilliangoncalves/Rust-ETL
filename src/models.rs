735727984b0d0fecad7b82ff558a6863e02b5a43
Willian Goncalves <williangoncalves74@gmail.com>
2026-01-13 19:19:50 -0300

    Adicionado a struct de config

---
+++ src/models.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+
+/// Representa a configuração global do pipeline de dados.
+///
+/// Esta estrutura é responsável por mapear o arquivo `config.json` para objetos em memória.
+/// Ela atua como a "Fonte da Verdade" para as URLs e recursos que serão processados.
+///
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Config {
+    /// A URL base (host + path inicial) da API.
+    pub base_url: String,
+
+    /// Um dicionário (Mapa) de endpoints a serem processados.
+    ///
+    /// * **Chave (Key):** O nome amigável do recurso. Será usado para nomear os arquivos
+    ///   de saída (ex: `raw_compras.json`, `compras.parquet`).
+    /// * **Valor (Value):** O caminho relativo (sufixo) da URL, incluindo query strings.
+    pub endpoints: HashMap<String, String>,
+}
